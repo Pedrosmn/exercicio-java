4d4ae771c80b6570c8f0541786291ad4b57b776a
Pedro <opedrosantos16@gmail.com>
2024-02-22 15:47:14 -0300

    Uso do while

---
+++ ex052.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ex052 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("----NÚMEROS PRIMOS----");
+        System.out.print("Digite um número: ");
+        int num = scan.nextInt();
+
+        int contador = 0;
+        int i = 1;
+        //int contador = 0;
+        while (i < (num+1)) {
+            if (num % i == 0) {
+                contador += 1;
+            }
+            i++;
+        }
+        if (contador > 2) {
+            System.out.println("O número " + num + " foi divisível " + contador + " vezes\n" +
+                               "por isso ele NÃO É PRIMO!!");
+        } else {
+            System.out.println("O número " + num + " foi divisível " + contador + " vezes\n" +
+                    "por isso ele É PRIMO!!");
+        }
+    }
+}
